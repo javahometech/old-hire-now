a357f3ad0298696160325d02dbe729f9441aac0f
Hari Kammana <hari.kammana@gmail.com>
2020-09-24 20:17:33 +0530

    posting job data into dynamodb

--- services/post-job/job.py
+++ services/post-job/job.py
@@ -1,17 +1,23 @@
 import boto3
 import json
 import os
+import uuid
 
 dynamodb = boto3.resource('dynamodb')
 table_name=os.environ['TABLE_NAME']
 table = dynamodb.Table(table_name)
 
 def post(event, context):
-    print(event)
     try:
+        job_id = str(uuid.uuid4())
+        event['job_id'] = job_id
+        table.put_item(
+            Item=event
+        )
         resp = {
             'statusCode':200,
-            "body": event
+            "message": "Job Posted Successfully",
+            "job_id": job_id
         }
         return json.dumps(resp)
     except Exception as e:
