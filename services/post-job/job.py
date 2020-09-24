9a6db625502ae333fa58af6e7a0ada5f30350545
Hari Kammana <hari.kammana@gmail.com>
2020-09-24 10:51:42 +0530

    serverless project setup

---
+++ services/post-job/job.py
@@ -0,0 +1,22 @@
+import boto3
+import json
+import os
+
+dynamodb = boto3.resource('dynamodb')
+table_name=os.environ['TABLE_NAME']
+table = dynamodb.Table(table_name)
+
+def add(event, context):
+    print(event)
+    try:
+        resp = {
+            'statusCode':200,
+            "body": event
+        }
+        return json.dumps(resp)
+    except Exception as e:
+        response = {
+            'statusCode': 500,
+            'message': str(e)
+        }
+        raise Exception(json.dumps(response))
